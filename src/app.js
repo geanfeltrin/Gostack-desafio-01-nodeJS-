d3617b0b92b9177fe4f2eb15269645bc64060b3c
Gean Feltrin <gean_costa@hotmail.com>
2020-05-04 08:52:23 -0300

    Add new repository

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-
-// const { uuid } = require("uuidv4");
+const { uuid } = require('uuidv4')
 
 const app = express();
 
@@ -11,11 +10,24 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  const allRepositories = repositories
+
+  return response.json(allRepositories)
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { url, title, techs } = request.body
+  
+  const repository = {
+    id: uuid(),
+    url,
+    title,
+    techs: techs,
+    like: 0
+  }
+  repositories.push(repository)
+  
+  return response.json(repository)  
 });
 
 app.put("/repositories/:id", (request, response) => {
